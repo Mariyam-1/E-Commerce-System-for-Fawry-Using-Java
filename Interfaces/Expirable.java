fd0fd56a5f169686dab918a87e5e341e67597e9f
Mariyam-1 <mariyammousa1@gmail.com>
2025-07-04 23:16:31 +0300

    Add files via upload

---
+++ Interfaces/Expirable.java
@@ -0,0 +1,4 @@
+package Interfaces;
+public interface Expirable {
+    boolean isExpired();
+}
---
+++ Interfaces/Shippable.java
@@ -0,0 +1,6 @@
+package Interfaces;
+
+public interface Shippable {
+    double getWeight();
+    String getName();
+}
