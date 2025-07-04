01bbb6249023ce9281752b37702d2771e7c3bae6
Mariyam-1 <mariyammousa1@gmail.com>
2025-07-04 23:11:44 +0300

    Delete NonShippableProducts.java

--- NonShippableProducts.java
+++
@@ -1,9 +0,0 @@
-public class NonShippableProducts extends Products
- {
-   
-    public NonShippableProducts(String name, double price, int quantity) 
-    {
-        super(name, price, quantity);
-    }
-
-}
